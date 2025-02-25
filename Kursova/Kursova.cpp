45f3fc8afdc3ba9d26aa8482f2e36042d201c1e3
Andriy <andrii.denysiuk-ki232@nung.edu.ua>
2025-02-25 21:59:56 +0200

    This is all I could do (mostly) myself

--- Kursova/Kursova.cpp
+++ Kursova/Kursova.cpp
@@ -4,10 +4,27 @@
 #include "Counters.cpp"
 #include <locale>
 #include <algorithm>
+#include <codecvt>
 
 using namespace std;
 
+bool isMultiByte(char c) {
+	if ((c & 0x80) == 0) return 1;   // ASCII (1 байт)
+	if ((c & 0xE0) == 0xC0) return 2; // 110xxxxx → 2 байти (кирилиця)
+	if ((c & 0xF0) == 0xE0) return 3; // 1110xxxx → 3 байти
+	if ((c & 0xF8) == 0xF0) return 4; // 11110xxx → 4 байти
+	return -1;  // Некоректний UTF-8 символ
+}
+std::wstring utf8_to_wstring(const std::string& str) {
+	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
+	return converter.from_bytes(str);
+}
 
+// Функція для конвертації std::wstring у std::string (UTF-8)
+std::string wstring_to_utf8(const std::wstring& wstr) {
+	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
+	return converter.to_bytes(wstr);
+}
 int main()
 {
 	system("chcp 65001");
@@ -52,6 +69,45 @@ int main()
 	/*for (char a : read){
 		manager->write();
 	}*/
+	string read2 = read;
+	string error = "";
+	string abetka = " абвгґдеєжзиіїйклмнопрстуфхцчьюяАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧЬЮЯ";
+	bool isWrong = true;
+	cout << abetka.length() << endl << read2.length();
+
+	for (int i = 0; i < read2.length() - 1; i++) {
+		if (isMultiByte(read2[i]) == 2) {
+			for (int j = 0; j < abetka.length() - 1; j ++/*= 2*/) {
+				if (j == 66) string p = read2.substr(j - 2, j + 2);
+				if (read2.substr(i, isMultiByte(read2[i])) == abetka.substr(j, isMultiByte(abetka[j]))/*read2[i] == abetka[j] && read2[i + 1] == abetka[j + 1]*/) {
+					isWrong = false;
+					break;
+				}
+				//cout << j << "end" << i << endl;
+			}
+			i++;
+		}
+		if (isMultiByte(read2[i]) == 3) {
+			for (int j = 0; j < abetka.length() - 1; j += 2) {
+				if (j == 66) string p = read2.substr(j - 2, j + 2);
+				if (read2[i] == abetka[j] && read2[i + 1] == abetka[j + 1]) {
+					isWrong = false;
+					break;
+				}
+				//cout << j << "end" << i << endl;
+			}
+			i += 2;
+		}
+		//cout << "END" << endl;
+		if (isWrong) {
+			error += "Помилка в " + read2.substr(i, read2.find(' ', i)) + ". Недійсний символ." + '\n';
+			//i = read2.find(' ', i);
+		}
+		isWrong = true;
+	}
+	cout << error << endl;
+	manager->write(error);
+
 	string voiceless = "Кількість слів, що починаються з глухих голосних літер: ";
 	string voiced = "Кількість слів, що починаються з дзвінких голосних літер: ";
 	string soft = "Кількість слів, що починаються з м'яких приголосних літер: ";
